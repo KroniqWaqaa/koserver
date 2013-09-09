5a246420e91367faf68cfc709ab64644ca0ac081
Bodom61 <extasyspike>
2013-09-10 01:19:51 +0300

    Soccer Lua Düzenlemesi

--- official_quests/29079_Soccer.lua
+++ official_quests/29079_Soccer.lua
@@ -10,17 +10,19 @@ if EVENT == 102 then
 end
 
 if EVENT == 103 then
+   ZoneChange(UID, 21, 672, 166);
    -- Enter the blue Team
 end
 
 if EVENT == 104 then
+   ZoneChange(UID, 21, 672, 154);
    -- Enter the red Team
 end
 
 if EVENT == 101 then
    GOAL = HowmuchItem(UID, 900349000);
    if GOAL >= 10 then
-   SelectMsg(UID, 2, -1, 9617, NPC, 4006, 105, 27, 240);
+   SelectMsg(UID, 3, -1, 9617, NPC, 7224, 105, 7225, 106, 7226, 107, 7227, 108);
    else
    SelectMsg(UID, 2, -1, 9616, NPC, 27, 240);
    end
@@ -31,13 +33,33 @@ if EVENT == 240 then
 end
 
 if EVENT == 105 then
-   RobItem(UID, 900349000, 10);
-   -- Uniforms Give
+   RobItem(UID, 900349000, 10)
+   GiveItem(UID, 900362641, 1)
+end
+
+if EVENT == 106 then
+   RobItem(UID, 900349000, 10)
+   GiveItem(UID, 929001609, 1)
+end
+
+if EVENT == 107 then
+   RobItem(UID, 900349000, 10)
+   GiveItem(UID, 933001634, 1)
+end
+
+if EVENT == 108 then
+   --RobItem(UID, 900349000, 10)
+   --GiveItem(UID, TYPE D, 1)
 end
 
 if EVENT == 200 then
    SelectMsg(UID, 2, -1, 9630, NPC, 27, 240);
 end
+
+
+
+
+
 
 
 
