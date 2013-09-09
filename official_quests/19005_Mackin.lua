b032d9d64d617d386e029ba3206e684a25ea5738
Bodom61 <extasyspike>
2013-09-09 21:14:57 +0300

    Upload

---
+++ official_quests/19005_Mackin.lua
@@ -0,0 +1,17 @@
+local NPC = 19005;
+
+local MONEY = 0;
+
+if EVENT == 100 then
+   MONEY = HowmuchItem(UID, 900000000);
+   if MONEY => 1000000 then
+      SelectMsg(UID, 3, -1, 8075, NPC, 4481, 205, 3019, 203);
+   else
+      SelectMsg(UID, 2, -1, 11338, NPC, 27);
+   end
+end
+
+if EVENT == 205 then
+   GoldLose(UID, 1000000)
+   SelectMsg(UID, 16, -1, NPC);
+end
---
+++ official_quests/31508_NEnchant.lua
@@ -0,0 +1,41 @@
+
+local UserClass;
+local QuestNum;
+local Ret = 0;
+local NPC = 31508;
+
+
+if EVENT == 100 then
+	SelectMsg(UID, 3, -1, 242, NPC, 7091, 7099, 7092, 7100, 7093, 7101);
+end
+
+if EVENT == 7099 then
+	SelectMsg(UID, 3, -1, 242, NPC, 7099, 666);
+end
+
+if EVENT == 7100 then
+	SelectMsg(UID, 3, -1, 242, NPC, 7099, 667);
+end
+
+if EVENT == 7101 then
+	SelectMsg(UID, 3, -1, 242, NPC, 7099, 668);
+end
+
+
+if EVENT == 666 then
+	CastSkill(UID,302327)
+	GoldLose(UID, 50000)
+	NpcMsg(UID, 9137)
+end
+
+if EVENT == 667 then
+	CastSkill(UID,302331)
+	GoldLose(UID, 50000)
+	NpcMsg(UID, 9137)
+end
+
+if EVENT == 668 then
+	CastSkill(UID,302328)
+	GoldLose(UID, 50000)
+	NpcMsg(UID, 9137)
+end
