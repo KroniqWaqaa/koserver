b756935a246815345ea3bc4c7ae9188a742554f7
bodom61 <seyma_88@hotmail.de>
2013-09-12 18:17:03 +0300

    Update 13013_Patric.lua

--- official_quests/13013_Patric.lua
+++ official_quests/13013_Patric.lua
@@ -110,13 +110,9 @@ if EVENT == 185 then
    end
 end
 
-
-
 if EVENT == 188 then
    ShowMap(UID, 1);
 end
-
-local Check;
 
 if EVENT == 177 then
    ExpChange(UID, 17)
@@ -186,8 +182,6 @@ if EVENT == 239 then
    ShowMap(UID, 7);
 end
 
-local Check;
-
 if EVENT == 238 then
   SaveEvent(UID, 65);
   ExpChange (UID, 150)
@@ -251,8 +245,6 @@ end
 if EVENT == 306 then
    ShowMap(UID, 11);
 end
-
-local Check;
 
 if EVENT == 307 then
    SaveEvent(UID, 3324);
@@ -321,8 +313,6 @@ if EVENT == 406 then
    ShowMap(UID, 34);
 end
 
-local Check;
-
 if EVENT == 407 then
    SaveEvent(UID, 3334);
    ExpChange(UID, 1167) 
@@ -369,7 +359,6 @@ if EVENT == 9267 then
    SaveEvent(UID, 5303);
 end
 
-
 if EVENT == 9273 then
    SelectMsg(UID, 2, 890, 3171, NPC, 10, 168);
 end
@@ -413,8 +402,6 @@ if EVENT == 506 then
    ShowMap(UID, 325);
 end
 
-local Check;
-
 if EVENT == 507 then
    SaveEvent(UID, 3344);
    ExpChange(UID, 1500)
@@ -447,8 +434,6 @@ if EVENT == 671 then
    SaveEvent(UID, 3353);
    SelectMsg(UID, 1, 323, 3182, NPC, 10, 168);
 end
-
-
 
 if EVENT == 672 then
    SelectMsg(UID, 1, 323, 3177, NPC, 24, 3002);
@@ -527,8 +512,6 @@ if EVENT == 606 then
    ShowMap(UID, 326);
 end
 
-local Check;
-
 if EVENT == 607 then
    SaveEvent(UID, 3354);
    ExpChange(UID, 2084)
@@ -597,8 +580,6 @@ if EVENT == 706 then
    ShowMap(UID, 12);
 end
 
-local Check;
-
 if EVENT == 707 then
    SaveEvent(UID, 3364);
    ExpChange(UID, 2084)
@@ -666,8 +647,6 @@ if EVENT == 806 then
    ShowMap(UID, 325);
 end
 
-local Check;
-
 if EVENT == 807 then
    SaveEvent(UID, 3374);
    ExpChange(UID, 4584)
@@ -706,7 +685,7 @@ if EVENT == 901 then
 end
 
 if EVENT == 902 then
-	SelectMsg(UID, 2, 320, 3818, NPC, 10, 168);
+   SelectMsg(UID, 2, 320, 3818, NPC, 10, 168);
    SaveEvent(UID, 3383);
 end
 
@@ -723,8 +702,8 @@ end
 local MonsterCount = 0;
 
 if EVENT == 905 then
-   MonsterCount  = CountMonsterQuestSub(UID,326, 1);
-   if  MonsterCount < 10 then
+   MonsterCount  = CountMonsterQuestSub(UID, 326, 1);
+   if MonsterCount < 10 then
       SelectMsg(UID, 2, 326, 3820, NPC, 10, 906);
    else
       SelectMsg(UID, 4, 326, 3823, NPC, 10, 907, 27, 906);
